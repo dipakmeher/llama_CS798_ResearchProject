82e6dbc11b7a16d935b546e01ab70489dff2fad6
“Dipak <dipakmeher54@gmail.com>
2024-05-02 20:49:46 -0400

    CS798 Research Project

---
+++ setup_huggingface.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export HF_HOME=/scratch/dmeher/.cache/huggingface
+export TRANSFORMERS_CACHE=/scratch/dmeher/.cache/huggingface/transformers
+export HF_DATASETS_CACHE=/scratch/dmeher/.cache/huggingface/datasets
+export HF_METRICS_CACHE=/scratch/dmeher/.cache/huggingface/metrics
+export HF_MODULES_CACHE=/scratch/dmeher/.cache/huggingface/modules
+#export HF_TOKEN='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'
+export HF_TOKEN='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'
